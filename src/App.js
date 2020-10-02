c735d002c07696b3511434c016b493fd50ffccc7
angelle rina <angellerinac@gmail.com>
2020-10-02 18:24:20 -0400

    Updated UseEffect direction in App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
     if (direction === "right") theLinkRef = linkRightRef;
 
     context.drawImage(theLinkRef.current, x, y);
-  }, [x, y]);
+  }, [x, y, direction]);
 
   return (
     <div className='app'>
